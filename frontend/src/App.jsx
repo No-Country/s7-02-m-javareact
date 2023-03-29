9fefb4ffac5958d1e648bd4e25614f25630d63a0
FarBusC4 <{ID}+{farbuscoder}@users.noreply.github.com>
2023-03-29 10:39:50 -0300

    Update Register Form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,80 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./views/Login";
-import Home from "./views/Home";
+
+//Components
+import Login from "./views/Auth/Login/Login";
+import Register from "./views/Auth/Register/Register";
+import Home from "./views/Home/Home";
 import AppNavbar from "./components/AppNavbar";
+
+
+//Framer motion
+import { AnimatePresence, motion } from "framer-motion";
+
+const pageTransition = {
+  in: {
+    opacity: 1,
+  },
+
+  out: {
+    opacity: 0,
+  },
+};
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
       <BrowserRouter>
+      <AnimatePresence>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+        <Route
+        path="/"
+        exact
+        element={
+          <motion.div
+            className="page"
+            initial="out"
+            animate="in"
+            exit="out"
+            variants={pageTransition}
+          >
+            <Home />
+          </motion.div>
+        }
+      ></Route>
+            <Route
+        path="/login"
+        exact
+        element={
+          <motion.div
+            className="page"
+            initial="out"
+            animate="in"
+            exit="out"
+            variants={pageTransition}
+          >
+            <Login/>
+          </motion.div>
+        }
+      ></Route>
+               <Route
+        path="/register"
+        exact
+        element={
+          <motion.div
+            className="page"
+            initial="out"
+            animate="in"
+            exit="out"
+            variants={pageTransition}
+          >
+            <Register />
+          </motion.div>
+        }
+      ></Route>
         </Routes>
+      </AnimatePresence>
       </BrowserRouter>
     </div>
   );
