29b116c273103d77b8e9aa30b29b8f6479148255
Cristian Gabriel <cristian_granero90@yahoo.com>
2023-03-29 10:19:29 -0300

    Place repository added

---
+++ backend/src/main/java/juntas/repository/PlaceRepository.java
@@ -0,0 +1,15 @@
+package juntas.repository;
+
+import juntas.model.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+    @Override
+    Optional<Place> findById(Long id);
+    @Override
+    List<Place> findAll();
+}
